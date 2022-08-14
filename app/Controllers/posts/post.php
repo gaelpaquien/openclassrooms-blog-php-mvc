86251edf886090f5c852d8ae7e1861e395e95d4b
Gaël Paquien <contact@gaelpaquien.com>
2022-08-15 00:51:52 +0200

    feat:(singlePost-#23): Create the template for the page displaying a single article

--- app/Controllers/posts/post.php
+++ app/Controllers/posts/post.php
@@ -1,7 +1,12 @@
 <?php
+
+use App\Core\Templating;
 use App\Models\GlobalQueries\PostGlobalQueries;
 
 $postsData = new PostGlobalQueries;
 $post = $postsData->find($params['id']);
+
+$twig = new Templating;
+$twig->template('posts/post.html.twig');
 
 require(ROOT . '/app/Views/posts/post.php');
