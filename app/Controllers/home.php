4e54258c0d16ed91c13ed99f61fea118dbb0eff5
Galuss <galuss@protonmail.com>
2022-08-16 17:07:45 +0200

    feat:(PostManagement-#13): Implementation of post management (CRUD and display on templates).

--- app/Controllers/home.php
+++ app/Controllers/home.php
@@ -1,7 +1,14 @@
 <?php
 use App\Core\Templating;
+use App\Models\Posts;
+
+$data = new Posts;
+$posts = $data->findAll(3);
 
 $twig = new Templating;
-$twig->view('pages/home.html.twig', []);
+$twig->view('pages/home.html.twig', [
+    'posts' => $posts, 
+    'urlAllPosts' => $router->url('posts')
+]);
 
 require(ROOT . '/app/Views/Home.php');
