5fe37456d814390ec70e5300f4b49033da7e25da
Galuss <galuss@protonmail.com>
2022-09-04 23:35:26 +0200

    feat:(articles-#13): Article management (creation, modification) and contact form

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -10,6 +10,27 @@ class MainController extends Controller
         $this->view('pages/home.html.twig', ['articles' => $data]);
     }
 
+    public function homeContact(): void 
+    {
+        if (isset($_POST) && !empty($_POST)) {
+            $to = $_POST['email']; 
+            $subject = $_POST['subject'];
+            $message = $_POST['message'];
+            $from = "gael.paquien.contact@gmail.com";
+            $headers = "From:" . $from;
+        
+            /* Il faut paramétrer un service mail, à voir avec hébergement Infomaniak
+            if (mail($to, $subject, $message, $headers)) {
+                echo "Mail Sent.";
+            }
+            else {
+                echo "failed";
+            }
+            */
+        }
+        header('Location: ' . '/#contact');
+    }
+
     public function termsOfUse(): void
     {
         $this->view('pages/others/termsOfUse.html.twig');
