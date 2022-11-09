cfaf490fbcd143677ca8f49022290582000a04dc
Gaël Paquien <contact@gaelpaquien.com>
2022-11-09 08:00:16 +0100

    Final adjustments

---
+++ app/Models/Contact/ContactModel.php
@@ -0,0 +1,155 @@
+<?php
+namespace App\Models\Contact;
+
+class ContactModel extends ContactManager
+{
+
+    protected int $id;
+    protected string $lastname;
+    protected string $firstname;
+    protected string $email;
+    protected string $subject;
+    protected string $message;
+    protected $sent_at;
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastname
+     */ 
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     *
+     * @return  self
+     */ 
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of firstname
+     */ 
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set the value of firstname
+     *
+     * @return  self
+     */ 
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of subject
+     */ 
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Set the value of subject
+     *
+     * @return  self
+     */ 
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of message
+     */ 
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set the value of message
+     *
+     * @return  self
+     */ 
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sent_at
+     */ 
+    public function getSent_at()
+    {
+        return $this->sent_at;
+    }
+
+    /**
+     * Set the value of sent_at
+     *
+     * @return  self
+     */ 
+    public function setSent_at($sent_at)
+    {
+        $this->sent_at = $sent_at;
+
+        return $this;
+    }
+}
