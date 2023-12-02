812516ce63cd04c94bd793a7352b21a3fd86c433
Gaël Paquien <contact@gaelpaquien.com>
2023-12-02 01:57:52 +0100

    Global improvements twig/css

--- public/assets/js/popup.js
+++ public/assets/js/popup.js
@@ -2,7 +2,7 @@
 const popup = document.getElementById('project-popup');
 const closeButton = document.getElementById('close-popup');
 const triggerButton = document.getElementById('popup-trigger');
-const header = document.getElementById('header');
+const navigation = document.getElementById('navigation-sticky');
 
 // For the first time, create the sessionStorage item
 if (sessionStorage.getItem('popupShown') === null) {
@@ -13,23 +13,23 @@ if (sessionStorage.getItem('popupShown') === null) {
 triggerButton.addEventListener('click', (e) => {
     e.preventDefault();
     popup.style.display = 'block';
-    header.style.position = 'static';
+    navigation.style.position = 'static';
     sessionStorage.setItem('popupShown', 'false');
 });
 
 // Event listener for the close button to hide the popup anc continue in the website
 closeButton.addEventListener('click', () => {
     popup.style.display = 'none';
-    header.style.position = 'fixed';
+    navigation.style.position = 'sticky';
     sessionStorage.setItem('popupShown', 'true');
 });
 
 // If the popup has not been shown yet, show it, else hide it
 if (sessionStorage.getItem('popupShown') === 'false') {
     popup.style.display = 'block';
-    header.style.position = 'static';
+    navigation.style.position = 'static';
 } else {
     const popup = document.getElementById('project-popup');
     popup.style.display = 'none';
-    header.style.position = 'fixed';
+    navigation.style.position = 'sticky';
 }
