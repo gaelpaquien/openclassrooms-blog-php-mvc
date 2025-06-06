6757d05b8a6d1767f10152b1518327365760ee1e
Gaël Paquien <contact@gaelpaquien.com>
2025-06-06 13:54:37 +0200

    Remove MySQL service in prod

--- app/docker/scripts/build.dev.sh
+++ app/docker/scripts/build.dev.sh
@@ -9,13 +9,6 @@ if [ ! -f .env.local ]; then
   cp .env .env.local || exit 1
 fi
 
-echo "Waiting for database..."
-while ! nc -z mysql 3306; do
-  echo "Waiting for MySQL to be ready..."
-  sleep 1
-done
-echo "Database is ready!"
-
 echo "Installing dependencies..."
 composer install || exit 1
 
