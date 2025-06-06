41b5593665b7bb9d6715a2730cc0235d55409e5e
Gaël Paquien <gael@plany.fr>
2025-06-06 14:00:49 +0200

    Fix build

--- app/docker/scripts/build.prod.sh
+++ app/docker/scripts/build.prod.sh
@@ -14,16 +14,17 @@ composer install || exit 1
 
 source .env.local
 
+MYSQL_DSN="-h ${DATABASE_HOST} -u ${MYSQL_USER} --password=${MYSQL_PASSWORD}"
+
 echo "Waiting for MySQL to be ready..."
-until mysql -h ${DATABASE_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "SELECT 1" &> /dev/null; do
+until mysql ${MYSQL_DSN} -e "SELECT 1" &> /dev/null; do
   echo "MySQL not ready, waiting..."
   sleep 2
 done
 
 echo "Setting up database..."
-mysql -h ${DATABASE_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} \
-  -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -h ${DATABASE_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/docker/scripts/init-db.sql
+mysql ${MYSQL_DSN} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql ${MYSQL_DSN} ${MYSQL_DATABASE} < app/docker/scripts/init-db.sql
 
 echo "Building app completed successfully!"
 
