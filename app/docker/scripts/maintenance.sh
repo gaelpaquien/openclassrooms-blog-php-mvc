001ba3ceb4bdae7e3f48434ae514dbdc04b7aef0
Gaël Paquien <contact@gaelpaquien.com>
2025-07-13 12:33:42 +0200

    Improve maintenance script

--- app/docker/scripts/maintenance.sh
+++ app/docker/scripts/maintenance.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
+set -e
 
-# Set working directory
-# cd /path/to/project
+log() {
+    echo "[$(date '+%H:%M:%S')] $1"
+}
 
-# Start of script
-echo "maintenance.sh: Start of daily maintenance script"
+log "START: Maintenance blogphpmvc"
 
-# Delete images added in the last 24 hours (including subdirectories)
-echo "maintenance.sh: Delete images added in the last 24 hours (including subdirectories)"
-find "$PWD/public/assets/img/articles" -type f -mtime -1 -print -exec rm {} \;
-
-# Set path to .env file
-ENV_FILE=".env"
-
-# Load environment variables from .env
-if [ -f "$ENV_FILE" ]; then
-    echo "maintenance.sh: Loading environment variables from $ENV_FILE"
-    source "$ENV_FILE"
+log "Cleaning images added in the last 24 hours"
+UPLOADS_PATH="public/assets/img/articles"
+if [[ -d "$UPLOADS_PATH" ]]; then
+    DELETED_COUNT=$(find "$UPLOADS_PATH" -type f -mtime -1 -delete -print | wc -l)
+    log "OK: $DELETED_COUNT files deleted in $UPLOADS_PATH"
 fi
 
-# Database reset
-echo "maintenance.sh: Database reset"
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$DATABASE_HOST" "$MYSQL_DATABASE" < init-db.sql
+ENV_FILE=".env"
 
-# Delete temporary files
-echo "maintenance.sh: Delete temporary files"
+log "Database reset"
+if [[ -f "$ENV_FILE" ]]; then
+    source "$ENV_FILE"
+    mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$DATABASE_HOST" "$MYSQL_DATABASE" < init-db.sql
+fi
+
+log "Cleaning cache"
 rm -rf tmp/cache/*
 
-# Send a confirmation email
-# subject="Blog PHP MVC - Daily maintenance"
-# message="The daily maintenance script completed successfully."
-# recipient="your@email.com"
-# echo "$message" | mail -s "$subject" "$recipient"
-
-# End of script
-echo "maintenance.sh: All steps are completed, end of daily maintenance script"
+log "END: Maintenance blogphpmvc completed"
